efa5312ae230d44c43f51c622cbf096cd939a580
AnhLe3698 <anhviet.le00@hotmail.com>
2022-07-29 04:20:34 -0600

    Added New pat /urls/:id

--- express_server.js
+++ express_server.js
@@ -30,3 +30,8 @@ app.get("/urls", (req, res) => {
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+
+app.get("/urls/:id", (req, res) => {
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  res.render("urls_show", templateVars);
+});
