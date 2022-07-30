e9de8b1afabcb71a09867697e36fc06a355e7a7a
AnhLe3698 <anhviet.le00@hotmail.com>
2022-07-30 03:32:44 -0600

    Adding a Url Submition form

--- express_server.js
+++ express_server.js
@@ -17,6 +17,8 @@ app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
 
+app.use(express.urlencoded({ extended: true }));
+
 // Sends urls as a Json object
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
@@ -31,7 +33,24 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
 });
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
+
+// Generate 6 random alphanumeric characters as a single string
+let generateRandomString = function() {
+  let randomNumbers = [];
+  for (let i = 0; i < 7; i++) {
+    randomNumbers.push(Math.floor(Math.random() * 62));
+  }
+};
